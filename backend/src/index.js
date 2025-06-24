9aada273659a2c9187a7a3c23093167fe1b20621
Harshit Patel <harshitp2704@gmail.com>
2025-06-24 10:58:08 +0530

    Project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,29 +1,31 @@
-
 import express from "express";
 import dotenv from "dotenv";
-import {connectDB} from "./lib/db.js"
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 import cors from "cors";
-import { app, server } from "./lib/socket.js";
+
 import path from "path";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
 dotenv.config();
 
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
-
-app.use(express.json({ limit: '50mb' })); // Increased from default 100kb
-app.use(express.urlencoded({ limit: '50mb', extended: true })); // Added for form data
+app.use(express.json());
 app.use(cookieParser());
 app.use(
-    cors({
-        origin:"http://localhost:5173",
-        credentials:true,
-    })
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
 );
 
-// Routes should come AFTER middleware
-app.use("/api/auth",authRoutes);
+app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
@@ -34,9 +36,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT=process.env.PORT;
-const __dirname = path.resolve();
-server.listen(PORT,()=>{
-    console.log("Server is running on PORT: "+PORT);
-    connectDB();
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
